9d5f50a598bcc4f590e11e2da0073dbea08b61ee
Kislaya1 <kislayapant@gmail.com>
2021-11-28 18:42:12 +0530

    Created Spring Boot App

---
+++ src/test/java/com/api/development/CollegeManagement/CollegeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.development.CollegeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
