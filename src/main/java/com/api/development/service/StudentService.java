aecd311a9b19bc6278f199de23e8469afc81b29c
Kislaya1 <kislayapant@gmail.com>
2021-12-14 16:32:41 +0530

    Resolved Update Api Issue

--- src/main/java/com/api/development/service/StudentService.java
+++ src/main/java/com/api/development/service/StudentService.java
@@ -40,32 +40,33 @@ public class StudentService {
       final String contactNumber,
       final String email) {
     int counter = 0;
-    Student studentData = studentRepository.findByIdAndEmail(studentId, email);
+    Optional<Student> studentData = studentRepository.findById(studentId);
+    Student student = studentData.get();
     if (Objects.isNull(studentData)) {
       return ResponseHandler.generateResponse(
           false, NOT_FOUND.getMsg(), STUDENT_NOT_PRESENT.getMsg(), null, HttpStatus.NOT_FOUND);
     }
     if (!Objects.isNull(firstName)
         && firstName.length() > 0
-        && !Objects.equals(studentData.getFirstName(), firstName)) {
-      studentData.setFirstName(firstName);
+        && !Objects.equals(student.getFirstName(), firstName)) {
+      student.setFirstName(firstName);
       counter++;
     }
     if (!Objects.isNull(lastName)
         && lastName.length() > 0
-        && !Objects.equals(studentData.getLastName(), lastName)) {
-      studentData.setLastName(lastName);
+        && !Objects.equals(student.getLastName(), lastName)) {
+      student.setLastName(lastName);
       counter++;
     }
     if (!Objects.isNull(contactNumber)
         && contactNumber.length() > 0
-        && !Objects.equals(studentData.getContactNumber(), contactNumber)) {
-      studentData.setContactNumber(contactNumber);
+        && !Objects.equals(student.getContactNumber(), contactNumber)) {
+      student.setContactNumber(contactNumber);
       counter++;
     }
     if (!Objects.isNull(email)
         && email.length() > 0
-        && !Objects.equals(studentData.getEmail(), email)) {
+        && !Objects.equals(student.getEmail(), email)) {
       Optional<Student> studentOptional = studentRepository.findByEmail(email);
       if (studentOptional.isPresent()) {
         return ResponseHandler.generateResponse(
@@ -75,7 +76,7 @@ public class StudentService {
             null,
             HttpStatus.BAD_REQUEST);
       }
-      studentData.setEmail(email);
+      student.setEmail(email);
       counter++;
     }
     if (counter == 0)
@@ -83,6 +84,6 @@ public class StudentService {
           false, BAD_REQUEST.getMsg(), NOTHING_TO_UPDATE.getMsg(), null, HttpStatus.BAD_REQUEST);
     else
       return ResponseHandler.generateResponse(
-          true, CREATED.getMsg(), UPDATE_SUCCESS.getMsg(), studentData, HttpStatus.CREATED);
+          true, CREATED.getMsg(), UPDATE_SUCCESS.getMsg(), student, HttpStatus.CREATED);
   }
 }
